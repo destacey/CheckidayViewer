b38a315e3f3ffa1d8d547e303eb547d87d3cd63d
Dannie Stacey <dstacey@outlook.com>
2021-10-24 16:52:12 -0500

    Added Swagger to the project.  Added a post build step to automatically generate the API Client file within the Client project based on the OpenApi specification.  Updated the blazor code to use the new typed client rather than httpclient with json deserialization.

--- CheckidayViewer/Client/Program.cs
+++ CheckidayViewer/Client/Program.cs
@@ -15,6 +15,7 @@ namespace CheckidayViewer.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<IHolidaysClient, HolidaysClient>();
 
             builder.Services.AddMudServices();
 
